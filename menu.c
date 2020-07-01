9c8aa5ade319a72b2b374618f891a72978afa739
mharasch <67681785+mharasch@users.noreply.github.com>
2020-07-01 10:47:14 +0200

    Add files via upload

--- menu.c
+++ menu.c
@@ -5,14 +5,7 @@
 #define ITEM_KOM1   1
 #define ITEM_KOM2   2
 #define ITEM_KOM3   3
-#define ITEM_KOM4   4
-#define ITEM_KOM5   5
-#define ITEM_KOM6   6
-#define ITEM_KOM7   7
-#define ITEM_KOM8   8
-#define ITEM_KOM9   9
-#define ITEM_KOM10  10
-#define ITEM_ENDE   11
+#define ITEM_ENDE   4
 
 
 
@@ -20,23 +13,7 @@ char * komMenu[]={"\n 0\tHilfetext\n",
                   " 1\t1.Kommando\n",
                   " 2\t2.Kommando\n",
                   " 3\t3.Kommando\n",
-                  " 4\t4.Tim Schneider\n",
-
-		  " 5\t5.Alexander Tröger\n",
-		  " 6\t6.Tim Weber\n",
-      " 7\t7. David Holzapfel\n",
-      " 8\t8. Jorin Spiller\n",
-      " 10\t10. Nils Schneider\n",
-                  " 10\a\tEnde\n"};
-
-		              " 5\t5.Alexander Tröger\n",
-		              " 6\t6.Tim Weber\n",
-                  " 7\t7. David Holzapfel\n",
-                  " 8\t8. Jorin Spiller\n",
-                  " 9\t9. Maurice Faber\n"
-                  " 11\a\tEnde\n"};
-
-
+                  " 4\a\tEnde\n"};
 int steuerMenu(void);
 
 int main(int argc, char* argv[])
@@ -69,18 +46,6 @@ int steuerMenu()
                          break;
       case  ITEM_KOM3:   printf("%s",komMenu[ITEM_KOM3]);
                          break;
-      case  ITEM_KOM4:   printf("%s",komMenu[ITEM_KOM4]);
-                         break;
-      case ITEM_KOM5:  printf("%s",komMenu[ITEM_KOM5]);
-			 break;
-      case ITEM_KOM6:  printf("%s",komMenu[ITEM_KOM6]);
-       break;
-      case ITEM_KOM7:  printf("%s",komMenu[ITEM_KOM7]);
-       break;
-      case ITEM_KOM8:  printf("%s",komMenu[ITEM_KOM8]);
-       break;
-      case ITEM_KOM9:  printf("%s",komMenu[ITEM_KOM9]);
-			 break;
       case  ITEM_ENDE:   printf("%s",komMenu[ITEM_ENDE]);
                          break;
       default: printf("\nkein Kommando\n");
