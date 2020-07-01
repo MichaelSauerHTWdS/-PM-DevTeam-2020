81da22b678ff32a8ab94602d3555cc887461c950
Tim <Tim@DESKTOP-KL75PVM.fritz.box>
2020-07-01 10:15:12 +0200

    Tim Schneider

--- menu.c
+++ menu.c
@@ -5,7 +5,8 @@
 #define ITEM_KOM1   1
 #define ITEM_KOM2   2
 #define ITEM_KOM3   3
-#define ITEM_ENDE   4
+#define ITEM_KOM4   4
+#define ITEM_ENDE   5
 
 
 
@@ -13,6 +14,7 @@ char * komMenu[]={"\n 0\tHilfetext\n",
                   " 1\t1.Kommando\n",
                   " 2\t2.Kommando\n",
                   " 3\t3.Kommando\n",
+                  " 3\t4.Tim Schneider\n",
                   " 4\a\tEnde\n"};
 int steuerMenu(void);
 
@@ -45,6 +47,8 @@ int steuerMenu()
       case  ITEM_KOM2:   printf("%s",komMenu[ITEM_KOM2]);
                          break;
       case  ITEM_KOM3:   printf("%s",komMenu[ITEM_KOM3]);
+                         break;
+      case  ITEM_KOM4:   printf("%s",komMenu[ITEM_KOM4]);
                          break;
       case  ITEM_ENDE:   printf("%s",komMenu[ITEM_ENDE]);
                          break;
